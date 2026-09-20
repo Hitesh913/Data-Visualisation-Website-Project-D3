79acc94034a970a475cea0014be41855cafd85c7
Hitesh913 <104788957@student.swin.edu.au>
2026-09-21 01:02:55 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,12 +5,10 @@ d3.select("#option2").on("click", function () {
     d3.select(".chartbox")
         .html(
             "<div class=\"hbar-charts\">" +
-                "<div id=\"chart-persons\"></div>" +
                 "<div id=\"chart-per1000\"></div>" +
             "</div>"
         );
 
-    drawHorizontalBarChartPersons("#chart-persons");
     drawHorizontalBarChartPer1000("#chart-per1000");
 
 });
